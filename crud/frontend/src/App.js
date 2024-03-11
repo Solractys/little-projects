e1eaf2d4479b461916daa04486a81a3749362772
Solractys <carloseduardosilvasouzadelima@gmail.com>
2024-03-11 01:35:03 +0000

    see you later ZZzZzZ

--- crud/frontend/src/App.js
+++ crud/frontend/src/App.js
@@ -1,12 +1,67 @@
 import './App.css';
-import React from 'react';
+import axios from 'axios'
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Conteudo</h1> 
-    </div>
-  );
+
+const baseUrl = 'http://localhost:3001/users'
+const initialState = {
+  user: { name: '', email: '' },
+  list: []
 }
 
-export default App;
+export default class App extends Component {
+
+  state = [...initialState]
+
+  clear() {
+    this.setState({ user: initialState })
+  }
+  save() {
+    const user = this.state.user
+    const method = user.id ? 'put' : 'post'
+    const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+    axios[method](url, user)
+      .then(resp => {
+        const list = this.getUpdateList(resp.data)
+        this.setState({ user: initialState, list })
+      })
+
+  }
+  getUpdateList(user) {
+    const list = this.state.list.filter(u => u.id !== user.id)
+    list.unshift(user)
+    return list
+  }
+  updateFiel(event) {
+    const user = { ...this.state.user }
+    user[event.target.name] = event.target.value
+    this.setState({ user })
+  }
+  renderForm() {
+    render(){
+      return (
+        <div className='form'>
+          <div className='row'>
+            <div className='col-12 col-md-6'>
+              <div className='form-group'>
+                <label>Nome
+                  <input type="text" className="form-control"
+                    value={this.state.user.name}
+                    onChange={e => this.updateFiel(e)}
+                    placeholder="Digite seu Nome"
+                  ></input>
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
+  render() {
+    return (
+      renderForm()
+    )
+  }
+}
