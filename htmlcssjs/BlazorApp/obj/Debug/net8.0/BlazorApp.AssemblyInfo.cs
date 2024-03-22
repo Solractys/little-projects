13e1c0b48198dd77ca3926bd2dd91be445b5adc6
Solractys <carloseduardosilvasouzadelima@gmail.com>
2024-03-21 21:53:26 -0300

    counter

--- htmlcssjs/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ htmlcssjs/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ff540db2a12d3bdeadfeb12c0cad9a1a5364c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71ec83ab48a619eec90ac0761610ff46b3d2b24b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
